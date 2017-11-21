1d4716f466d907ae3581d5e0e9ce8c31135d6466
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 01:04:04 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def keys_of(*arguments)
     hash = {"sugar glider"=>"Australia", "aye-aye"=> "Madagascar", "red-footed tortoise"=>"Panama", "kangaroo"=> "Australia", "tomato frog"=>"Madagascar", "koala"=>"Australia", a: 1, b: 2, c: 3, d: 1}
-    arguments.map { |arg| hash.select {|k,v| k if v == arg }.keys }.flatten
+    arguments.map { |arg| hash.map {|k,v| k if v == arg } }.flatten.compact
   end
 end

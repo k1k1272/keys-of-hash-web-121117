b7293d341589f11deb6dfd873cd6f0e5ae9633cd
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 00:46:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def keys_of(*arguments)
-    hash = {"Panama" => "red-footed tortoise" }
+    hash = {"Panama" => "red-footed tortoise", "Madagascar" => ["sugar glider", "kangaroo", "koala"]  }
     arguments.map { |arg| hash[arg]}
   end
 end
